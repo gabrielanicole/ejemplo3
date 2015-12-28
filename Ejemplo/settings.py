95b00c3ef27a96c94ad5a1e8b4caf04e0b665c63
gabrielanicole <gabriela.gonzalezsaez@gmail.com>
2015-12-28 12:10:06 -0300

    commit3

--- Ejemplo/settings.py
+++ Ejemplo/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'Ejemplo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
